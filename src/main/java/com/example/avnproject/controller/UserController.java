c9bbfc7ed54e55c0f7e07c0d30b78e1a637330f5
bakay <bakayberdibekov@gmail.com>
2021-11-16 00:52:50 +0300

    add user controller

---
+++ src/main/java/com/example/avnproject/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.example.avnproject.controller;
+
+import com.example.avnproject.converter.UserConverter;
+import com.example.avnproject.entity.User;
+import com.example.avnproject.model.ResponseMessage;
+import com.example.avnproject.model.UserModel;
+import com.example.avnproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired(required = false)
+    private UserService userService;
+
+    @GetMapping("/get-all")
+    public ResponseMessage<List<User>> getAll() {
+        return new ResponseMessage<List<User>>().prepareSuccessMessage(userService.getAll());
+    }
+
+    @PostMapping("/create-user")
+    public ResponseMessage<UserModel> createUser(@RequestBody UserModel userModel) {
+        ResponseMessage<UserModel> responseMessage = new ResponseMessage<>();
+        User user = new UserConverter().convertFromModel(userModel);
+        try {
+            return responseMessage
+                    .prepareSuccessMessage(new UserConverter()
+                            .convertToEntity(userService.create(user)));
+        } catch (IllegalArgumentException e) {
+            return responseMessage.prepareFailMessage(e.getMessage());
+        } catch (Exception e) {
+            return responseMessage.prepareFailMessage(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseMessage<User> getById(@PathVariable Long id) {
+        return new ResponseMessage<User>().prepareSuccessMessage(userService.getById(id));
+    }
+
+    @PutMapping("/update-user")
+    public ResponseMessage<User> updateUser(@RequestBody User user) {
+        return new ResponseMessage<User>().prepareSuccessMessage(userService.update(user));
+    }
+
+    @DeleteMapping("/delete-user")
+    public ResponseMessage<User> deleteById(@PathVariable Long id) {
+        return new ResponseMessage<User>().prepareSuccessMessage(userService.deleteById(id));
+    }
+}
---
+++ src/test/java/com/example/avnproject/AvnProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.avnproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvnProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
